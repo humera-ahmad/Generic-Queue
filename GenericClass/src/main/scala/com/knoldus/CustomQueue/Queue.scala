24a7069534cb0390f86bd85add51b8106e4d4552
Humera <76787291+humera-ahmad@users.noreply.github.com>
2021-03-18 11:55:14 +0530

    Update Queue.scala

--- GenericClass/src/main/scala/com/knoldus/CustomQueue/Queue.scala
+++ GenericClass/src/main/scala/com/knoldus/CustomQueue/Queue.scala
@@ -7,8 +7,8 @@ class Queue[+A](list:List[A]) {
 
   override def toString: String = list.toString
 
-  def enqueue[B>:A](x:B):Queue[B]= {        // using lower-type-bound
-    new Queue[B](list :+ x)
+  def enqueue[B>:A](input:B):Queue[B]= {        // using lower-type-bound
+    new Queue[B](list :+ input)
   }
 
   def dequeue[B>:A]:Try[Queue[B]] ={
